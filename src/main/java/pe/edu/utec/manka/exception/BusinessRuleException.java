b201660d38a96325e9061a4952c0e7db0cf6dd71
fran-li <francisco.lira@utec.edu.pe>
2026-09-25 17:33:57 -0500

    clear

--- src/main/java/pe/edu/utec/manka/exception/BusinessRuleException.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.utec.manka.exception;
-
-public class BusinessRuleException extends RuntimeException {
-
-    public BusinessRuleException(String message) {
-        super(message);
-    }
-}
--- src/main/java/pe/edu/utec/manka/exception/ConflictException.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.utec.manka.exception;
-
-public class ConflictException extends RuntimeException {
-
-    public ConflictException(String message) {
-        super(message);
-    }
-}
